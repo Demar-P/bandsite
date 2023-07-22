40d03a84f7773a530d0bc61a0e9fd0724517854c
Demar Porter <demarporter@Demar-Macbook-Pro.local>
2023-07-22 15:10:31 -0400

    created axios get request verified in console

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,22 +1,27 @@
 let comments = [
-    {
-        name: "Connor Walton",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-        date: "02/17/2021",
-    },
-    {
-        name: "Emilie Beach",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-        date: "01/09/2021",
-    },
-    {
-        name: "Miles Acosta",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: "12/20/2020",
-    },
+    // {
+    //     name: "Connor Walton",
+    //     comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+    //     date: "02/17/2021",
+    // },
+    // {
+    //     name: "Emilie Beach",
+    //     comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+    //     date: "01/09/2021",
+    // },
+    // {
+    //     name: "Miles Acosta",
+    //     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    //     date: "12/20/2020",
+    // },
 ]
+//creating axios get to update array 
 
-axois
+axios.get('https://project-1-api.herokuapp.com/comments?api_key=e5c0f0c1-2a94-4c3f-8166-06280b36bfb6')
+    .then(results => {
+    console.log (results.data);
+    results = comments
+})
 
 const commentContainer = document.querySelector('.comment-container');
 const commentForm = document.querySelector('.comment-new__form');
@@ -67,6 +72,8 @@ function displayComment() {
 }
 
 
+//posting comment data to api
+
 commentForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -96,3 +103,5 @@ commentForm.addEventListener('submit', (event) => {
 
 
 displayComment();
+
+
